aabdbe41305b3cb1205b1f60003aa54293e93036
Arto Bendiken <arto@bendiken.net>
2014-04-06 01:59:58 +0200

    Defined the <dry/bitops.h> header file.

---
+++ src/dry/bitops.h
@@ -0,0 +1,14 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef DRY_BITOPS_H
+#define DRY_BITOPS_H
+
+/**
+ * @file
+ */
+
+namespace dry {
+  // TODO
+}
+
+#endif /* DRY_BITOPS_H */
