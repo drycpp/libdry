6afb681ebdf2ab359c0708d97887d8e15d3950bf
Arto Bendiken <arto@bendiken.net>
2014-04-06 02:24:10 +0200

    Implemented the dry::popcount64() function.

--- src/dry/bitops.h
+++ src/dry/bitops.h
@@ -7,8 +7,18 @@
  * @file
  */
 
+#include <cstdint> /* for std::uint64_t */
+
 namespace dry {
-  // TODO
+  /**
+   * Returns the number of 1-bits in `word`.
+   */
+  static inline unsigned int
+  popcount64(const std::uint64_t word) {
+    static_assert(sizeof(word) <= sizeof(unsigned long long),
+      "__builtin_popcountll() word size mismatch");
+    return __builtin_popcountll(word);
+  }
 }
 
 #endif /* DRY_BITOPS_H */
--- test/check-bitops.cc
+++ test/check-bitops.cc
@@ -9,3 +9,12 @@
 #include <dry/bitops.h>
 
 using namespace dry;
+
+TEST_CASE("popcount64()") {
+  REQUIRE(popcount64(0b0) == 0);
+  REQUIRE(popcount64(0b1) == 1);
+  REQUIRE(popcount64(0b11) == 2);
+  REQUIRE(popcount64(0b111) == 3);
+  REQUIRE(popcount64(0b100) == 1);
+  REQUIRE(popcount64(~0ULL) == 64);
+}
