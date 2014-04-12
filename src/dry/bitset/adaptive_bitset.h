b3f327b0587bbff3bfc557d7b40fd840a53b873a
Arto Bendiken <arto@bendiken.net>
2014-04-12 23:21:32 +0200

    Defined the dry::adaptive_bitset class.

---
+++ src/dry/bitset/adaptive_bitset.h
@@ -0,0 +1,229 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef DRY_ADAPTIVE_BITSET_H
+#define DRY_ADAPTIVE_BITSET_H
+
+/**
+ * @file
+ */
+
+#include "../bitset.h"
+#include "empty_bitset.h"
+#include "small_bitset.h"
+#include "uniform_bitset.h"
+
+#include <memory> /* for std::unique_ptr */
+
+namespace dry {
+  class adaptive_bitset;
+}
+
+/**
+ * An adaptive bitset.
+ */
+class dry::adaptive_bitset final : public dry::bitset<adaptive_bitset> {
+  struct interface;
+  std::unique_ptr<interface> _impl;
+
+public:
+  /**
+   * Default constructor.
+   */
+  adaptive_bitset()
+    : _impl{new empty_implementation} {}
+
+  /**
+   * Constructor.
+   */
+  adaptive_bitset(std::size_t size)
+    : _impl{new small_implementation{size}} {}
+
+  /**
+   * Copy constructor.
+   */
+  adaptive_bitset(const adaptive_bitset& other) = default;
+
+  /**
+   * Move constructor.
+   */
+  adaptive_bitset(adaptive_bitset&& other) noexcept = default;
+
+  /**
+   * Destructor.
+   */
+  ~adaptive_bitset() noexcept = default;
+
+  /**
+   * Copy assignment operator.
+   */
+  adaptive_bitset& operator=(const adaptive_bitset& other) = default;
+
+  /**
+   * Move assignment operator.
+   */
+  adaptive_bitset& operator=(adaptive_bitset&& other) noexcept = default;
+
+  /**
+   * @copydoc bitset::empty()
+   */
+  bool empty() const noexcept {
+    return size() == 0;
+  }
+
+  /**
+   * @copydoc bitset::size()
+   */
+  std::size_t size() const noexcept {
+    return _impl->size();
+  }
+
+  /**
+   * @copydoc bitset::count()
+   */
+  std::size_t count(bool value = true) const noexcept {
+    return _impl->count(value);
+  }
+
+  /**
+   * @copydoc bitset::all()
+   */
+  bool all() const noexcept {
+    return _impl->all();
+  }
+
+  /**
+   * @copydoc bitset::any()
+   */
+  bool any() const noexcept {
+    return _impl->any();
+  }
+
+  /**
+   * @copydoc bitset::none()
+   */
+  bool none() const noexcept {
+    return _impl->none();
+  }
+
+  /**
+   * @copydoc bitset::test()
+   */
+  bool test(std::size_t pos) const {
+    return _impl->test(pos);
+  }
+
+  /**
+   * @copydoc bitset::operator[]()
+   */
+  bool operator[](std::size_t pos) const noexcept {
+    return _impl->operator[](pos);
+  }
+
+  /**
+   * @copydoc bitset::set()
+   */
+  adaptive_bitset& set() noexcept {
+    return _impl->set(), *this;
+  }
+
+  /**
+   * @copydoc bitset::set(std::size_t,bool)
+   */
+  adaptive_bitset& set(std::size_t pos, bool value = true) {
+    return _impl->set(pos, value), *this;
+  }
+
+  /**
+   * @copydoc bitset::reset()
+   */
+  adaptive_bitset& reset() noexcept {
+    return _impl->reset(), *this;
+  }
+
+  /**
+   * @copydoc bitset::reset(std::size_t)
+   */
+  adaptive_bitset& reset(std::size_t pos) {
+    return set(pos, false);
+  }
+
+  /**
+   * @copydoc bitset::flip()
+   */
+  adaptive_bitset& flip() noexcept {
+    return _impl->flip(), *this;
+  }
+
+private:
+  struct interface {
+    virtual ~interface() noexcept = default;
+    virtual std::size_t size() const noexcept = 0;
+    virtual std::size_t count(bool value = true) const noexcept = 0;
+    virtual bool all() const noexcept = 0;
+    virtual bool any() const noexcept = 0;
+    virtual bool none() const noexcept = 0;
+    virtual bool test(std::size_t pos) const = 0;
+    virtual bool operator[](std::size_t pos) const noexcept = 0;
+    virtual void set() noexcept = 0;
+    virtual void set(std::size_t pos, bool value = true) = 0;
+    virtual void reset() noexcept = 0;
+    virtual void flip() noexcept = 0;
+  };
+
+  template <class Bitset>
+  struct implementation : public interface {
+    Bitset self;
+    implementation() noexcept = default;
+    implementation(std::size_t size) noexcept : self{size} {}
+    virtual ~implementation() noexcept override = default;
+    virtual std::size_t size() const noexcept override {
+      return self.size();
+    }
+    virtual std::size_t count(bool value = true) const noexcept override {
+      return self.count(value);
+    }
+    virtual bool all() const noexcept override {
+      return self.all();
+    }
+    virtual bool any() const noexcept override {
+      return self.any();
+    }
+    virtual bool none() const noexcept override {
+      return self.none();
+    }
+    virtual bool test(std::size_t pos) const override {
+      return self.test(pos);
+    }
+    virtual bool operator[](std::size_t pos) const noexcept override {
+      return self.operator[](pos);
+    }
+    virtual void set() noexcept override {
+      self.set();
+    }
+    virtual void set(std::size_t pos, bool value = true) override {
+      self.set(pos, value);
+    }
+    virtual void reset() noexcept override {
+      self.reset();
+    }
+    virtual void flip() noexcept override {
+      //self.flip();
+    }
+  };
+
+  struct empty_implementation final : public implementation<empty_bitset> {
+    //using implementation<empty_bitset>::implementation;
+  };
+
+  struct small_implementation final : public implementation<small_bitset<>> {
+    small_implementation(std::size_t size) noexcept
+      : implementation<small_bitset<>>{size} {}
+  };
+
+  struct uniform_implementation final : public implementation<uniform_bitset> {
+    uniform_implementation(std::size_t size) noexcept
+      : implementation<uniform_bitset>{size} {}
+  };
+};
+
+#endif /* DRY_ADAPTIVE_BITSET_H */
--- test/check-bitset.cc
+++ test/check-bitset.cc
@@ -7,6 +7,7 @@
 #include "catch.hpp"
 
 #include <dry/bitset.h>
+#include <dry/bitset/adaptive_bitset.h>
 #include <dry/bitset/empty_bitset.h>
 #include <dry/bitset/small_bitset.h>
 #include <dry/bitset/uniform_bitset.h>
@@ -225,4 +226,92 @@ TEST_CASE("uniform_bitset#flip") {
   REQUIRE_NOTHROW(uniform_bitset{}.flip());
   REQUIRE(uniform_bitset{}.value() == false);
   REQUIRE(uniform_bitset{}.flip().value() == true);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/* dry::adaptive_bitset */
+
+using adaptive_bitset = dry::adaptive_bitset;
+
+TEST_CASE("adaptive_bitset#size") {
+  REQUIRE(adaptive_bitset{}.size() == 0);
+  REQUIRE(adaptive_bitset{1}.size() == 1);
+  REQUIRE(adaptive_bitset{100}.size() == 100);
+  REQUIRE(adaptive_bitset{4096}.size() == 4096);
+}
+
+TEST_CASE("adaptive_bitset#count") {
+  REQUIRE(adaptive_bitset{}.count(true) == 0);
+  REQUIRE(adaptive_bitset{}.count(false) == 0);
+  REQUIRE(adaptive_bitset{1}.count(true) == 0);
+  REQUIRE(adaptive_bitset{1}.count(false) == 1);
+}
+
+TEST_CASE("adaptive_bitset#all") {
+  REQUIRE(adaptive_bitset{}.all() == false);
+  REQUIRE(adaptive_bitset{1}.all() == false);
+  REQUIRE(adaptive_bitset{1}.set(0).all() == true);
+  REQUIRE(adaptive_bitset{2}.set(0).all() == false);
+  REQUIRE(adaptive_bitset{2}.set(1).all() == false);
+}
+
+TEST_CASE("adaptive_bitset#any") {
+  REQUIRE(adaptive_bitset{}.any() == false);
+  REQUIRE(adaptive_bitset{1}.any() == false);
+  REQUIRE(adaptive_bitset{1}.set(0).any() == true);
+  REQUIRE(adaptive_bitset{2}.set(0).any() == true);
+  REQUIRE(adaptive_bitset{2}.set(1).any() == true);
+}
+
+TEST_CASE("adaptive_bitset#none") {
+  REQUIRE(adaptive_bitset{}.none() == true);
+  REQUIRE(adaptive_bitset{1}.none() == true);
+  REQUIRE(adaptive_bitset{1}.set(0).none() == false);
+  REQUIRE(adaptive_bitset{2}.set(0).none() == false);
+  REQUIRE(adaptive_bitset{2}.set(1).none() == false);
+}
+
+TEST_CASE("adaptive_bitset#test") {
+  REQUIRE_THROWS_AS(adaptive_bitset{}.test(0), std::out_of_range);
+  REQUIRE(adaptive_bitset{1}.test(0) == false);
+  REQUIRE(adaptive_bitset{1}.set(0).test(0) == true);
+  REQUIRE(adaptive_bitset{2}.set(0).test(0) == true);
+  REQUIRE(adaptive_bitset{2}.set(1).test(1) == true);
+}
+
+TEST_CASE("adaptive_bitset#set") {
+  for (auto size = 0U; size < 16; size++) {
+    adaptive_bitset bits{size};
+    bits.set();
+    for (auto pos = 0U; pos < size; pos++) {
+      REQUIRE(bits.test(pos) == true);
+    }
+  }
+}
+
+TEST_CASE("adaptive_bitset#set(pos)") {
+  REQUIRE_THROWS_AS(adaptive_bitset{}.set(0), std::out_of_range);
+  REQUIRE(adaptive_bitset{1}.reset().set(0).test(0) == true);
+}
+
+TEST_CASE("adaptive_bitset#reset") {
+  for (auto size = 0U; size < 16; size++) {
+    adaptive_bitset bits{size};
+    bits.set().reset();
+    for (auto pos = 0U; pos < size; pos++) {
+      REQUIRE(bits.test(pos) == false);
+    }
+  }
+  REQUIRE_NOTHROW(adaptive_bitset{16}.reset());
+}
+
+TEST_CASE("adaptive_bitset#reset(pos)") {
+  REQUIRE_THROWS_AS(adaptive_bitset{}.reset(0), std::out_of_range);
+  REQUIRE(adaptive_bitset{1}.set().reset(0).test(0) == false);
+  REQUIRE(adaptive_bitset{2}.set().reset(0).test(1) == true);
+  REQUIRE(adaptive_bitset{3}.set().reset(0).count() == 2);
+}
+
+TEST_CASE("adaptive_bitset#flip") {
+  // TODO
 }
